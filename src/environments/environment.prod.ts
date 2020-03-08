b976ea57fb37e1e2d0671fd111c8003ecc4938b2
Hiten Patel <contact@hitenpatel.net>
2020-03-08 18:38:04 -0400

    fix pkce redirect url for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
     authorizationPkce: 'https://m3.auth.us-east-1.amazoncognito.com/oauth2/authorize?response_type=code&client_id=4mstutl93bnan7gnrdff6t7ado&redirect_uri=https://m3.hitenpatel.net/user&state={{STATE}}&code_challenge_method=S256&code_challenge={{CODE_CHALLENGE}}',
     token: 'https://m3.auth.us-east-1.amazoncognito.com/oauth2/token',
     marketData: 'http://localhost:9001/getMarketData/',
-    redirect: 'http://localhost:4200/user',
+    redirect: 'https://m3.hitenpatel.net/user',
   },
   idpClientId: '4mstutl93bnan7gnrdff6t7ado',
 };
